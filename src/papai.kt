734d9b71fa11b45092851c074e9c8a494cf0f5b3
Florencekyarikunda <florencekyarikund@gmail.com>
2021-02-25 21:36:28 +0300

    first commit

---
+++ src/papai.kt
@@ -0,0 +1,18 @@
+fun main() {
+add()
+    popel()
+    add()
+
+}
+fun add() {
+    var apple =20
+    var orange =10
+    var total =apple+orange
+    println(total)
+}
+fun popel (){
+    println("popel")
+}
+fun add(a:Int,b:Int):Int{
+    var total =return a+b
+}
